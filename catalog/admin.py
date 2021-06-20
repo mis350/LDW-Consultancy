7739295a5d377a0b5266350f4a25365503517e32
dalalajmi <82080292+dalalajmi@users.noreply.github.com>
2021-06-20 21:37:52 +0000

    created models and registered them

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookStatus, Reader, Review
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookStatus)
+admin.site.register(Reader)
+admin.site.register(Review)
