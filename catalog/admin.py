d31dc29447c70e92dee15aba2fc30ea97a675df2
lamasalajmi <82085905+lamasalajmi@users.noreply.github.com>
2021-06-22 21:52:41 +0000

    registered model admins + in line

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
-
+from .models import Book, Author, BookStatus, Reader, Genre, Review
 # Register your models here.
+
+class BooksInline(admin.TabularInline):
+  model = Book
+class AuthorAdmin(admin.ModelAdmin):
+  list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+  fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+admin.site.register(Author, AuthorAdmin)
+inlines = [BooksInline]
+
+class BookStatusInline(admin.TabularInline):
+  model = BookStatus
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+  list_display = ('title', 'author', 'display_genre')
+  inlines = [BookStatusInline]
+
+
+@admin.register(BookStatus)
+class BookStatusAdmin(admin.ModelAdmin):
+  list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+  list_filter = ('status', 'due_back')
+
+  fieldsets = (
+    (None, {'fields': ('book', 'imprint', 'id')}),
+    ('Availability', {'fields': ('status', 'due_back', 'borrower')}),
+  )
+
+
+
+
+
+
+
+
+
+
+
