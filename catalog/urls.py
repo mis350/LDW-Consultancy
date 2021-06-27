791bc546253d1c1cedad9ce83bc71ec0a05f7ccb
dalalajmi <82080292+dalalajmi@users.noreply.github.com>
2021-06-27 11:36:21 +0000

    added 4 views

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('review/<int:s>/',views.show_review, name="show-review"),
+    path('create/review/', views.create_review, name="create-review"),
 ]
+
